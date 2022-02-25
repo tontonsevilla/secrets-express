bfd31e170f6092aad4bde4baf16a1990105112a8
Winston Sevilla <winston_sevilla@outlook.com>
2022-02-25 11:23:34 +0800

    installed modules

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+let port = 3000;
+app.listen(port, console.log(`Server is now listening to port ${port}`));
